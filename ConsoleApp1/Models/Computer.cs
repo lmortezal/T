62a3818cd733b794bbd0406692c0bb4e235a9f73
morteza <lmortezal.world@gmail.com>
2024-03-09 20:26:41 +0330

    T

---
+++ ConsoleApp1/Models/Computer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Computer
+    {
+        public string Motherboard { get; set; }
+        public int CPUCores { get; set; }
+        public bool HasWifi { get; set; }
+        public bool HasLTE { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string VideoCard { get; set; }
+
+        public Computer()
+        {
+            if (VideoCard == null)
+            {
+                VideoCard = "";
+
+            }
+            if (Motherboard == null)
+            {
+                Motherboard = "";
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using ConsoleApp1.Models;
+using Dapper;
+
+namespace ConsoleApp1{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=True;Trusted_Connection=True;";
+            IDbConnection dbConnection = new SqlConnection(connectionString); 
+
+            string sqlcommand = "SELECT GETDATE()";
+
+            DateTime time = dbConnection.QuerySingle<DateTime>(sqlcommand);
+            Console.WriteLine(time);
+
+
+
+
+
+            Computer computer = new Computer()
+            {
+                Motherboard = "ASUS",
+                CPUCores = 4,
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = new DateTime(2020, 1, 1),
+                Price = 1000,
+                VideoCard = "NVIDIA"
+            };
+            Console.WriteLine(computer.Motherboard);
+            Console.WriteLine(computer.CPUCores);
+            Console.WriteLine(computer.HasWifi);
+            Console.WriteLine(computer.HasLTE);
+            Console.WriteLine(computer.ReleaseDate);
+            Console.WriteLine(computer.Price);
+            Console.WriteLine(computer.VideoCard);
+
+            string sql = @"INSERT INTO  TutorialAppSchema.Computer (
+                Motherboard,
+                CPUCores,
+                HasWifi,
+                HasLTE,
+                ReleaseDate, 
+                Price,
+                VideoCard
+            ) VALUES ('" + computer.Motherboard
+                + "','" + computer.CPUCores
+                + "','" + computer.HasWifi
+                + "','" + computer.HasLTE
+                + "','" + computer.ReleaseDate
+                + "','" + computer.Price
+                + "','" + computer.VideoCard
+            + "') ";
+
+            Console.WriteLine(sql);
+
+            string sqlselect = @"
+            SELECT
+                computer.Motherboard,
+                computer.CPUCores,
+                computer.HasWifi,
+                computer.HasLTE,
+                computer.ReleaseDate,
+                computer.Price,
+                computer.VideoCard
+            FROM TutorialAppSchema.Computer computer";
+
+            List<Computer> computers = dbConnection.Query<Computer>(sqlselect).ToList();
+
+            foreach (Computer item in sql)
+            {
+                Console.WriteLine(computer.Motherboard);
+                Console.WriteLine(computer.CPUCores);
+                Console.WriteLine(computer.HasWifi);
+                Console.WriteLine(computer.HasLTE);
+                Console.WriteLine(computer.ReleaseDate);
+                Console.WriteLine(computer.Price);
+                Console.WriteLine(computer.VideoCard);
+            }
+        }
+    }
+
+}
